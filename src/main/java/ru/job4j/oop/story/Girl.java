d125eff2a654bc0c89b5a115d590d9efacf10b6e
Slevkelebr <slevkelebr@yandex.ru>
2020-03-23 12:39:53 +0500

    Взаимодействие объектов. [#166087]

---
+++ src/main/java/ru/job4j/oop/story/Girl.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop.story;
+
+/**
+ * class Girl - красная шапочка, может позвать на помощь пионера.
+ *
+ * @author Sergey Frolov (slevkelebr@yandex.ru)
+ * @version 0.1
+ * @since 23.03.2020
+ */
+public class Girl {
+    public void help(Pioneer pioneer) {
+
+    }
+}
---
+++ src/main/java/ru/job4j/oop/story/Wolf.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop.story;
+
+/**
+ * class Wolf - охотся за красной шапочкой, может её съесть.
+ *
+ * @author Sergey Frolov (slevkelebr@yandex.ru)
+ * @version 0.1
+ * @since 23.03.2020
+ */
+public class Wolf {
+    public void eat(Girl girl) {
+
+    }
+}
