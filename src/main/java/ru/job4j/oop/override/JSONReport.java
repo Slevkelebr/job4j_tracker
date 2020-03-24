bb33df158b2f35dca13704447c3470c2ef00832b
Slevkelebr <slevkelebr@yandex.ru>
2020-03-24 10:21:00 +0500

    Переопределение [#174890]

---
+++ src/main/java/ru/job4j/oop/override/JSONReport.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop.override;
+
+/**
+ * class JSONReport.
+ *
+ * @author Sergey Frolov (slevkelebr@yandex.ru)
+ * @version 0.1
+ * @since 24.03.2020
+ */
+public class JSONReport {
+
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + System.lineSeparator() +
+                "name: " + name + "," + System.lineSeparator() +
+                "body: " + body + System.lineSeparator() + System.lineSeparator() + "}";
+    }
+}
