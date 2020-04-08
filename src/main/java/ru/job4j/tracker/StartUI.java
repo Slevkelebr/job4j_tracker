fa632a61243bdb3c19c1c9f91652ceae60f98978
Slevkelebr <slevkelebr@yandex.ru>
2020-04-09 02:46:17 +0500

    Статические методы. [#181779](разбил код на функциональные блоки. Это позволит при внесении правок в код сфокусироваться только на одном методе)

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -20,44 +20,98 @@ public class StartUI {
             this.showMenu();
             int select = input.askInt("Select: ");
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("=== All Item ====");
-                Item[] items = tracker.findAll();
-                for (Item item : items) {
-                    System.out.println(item.getName() + " " + item.getId());
-                }
+                StartUI.showAllItem(tracker);
             } else if (select == 2) {
-                String id = input.askStr("Enter ID item which want to replace: ");
-                String name = input.askStr("Enter new name item: ");
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Operation completed successfully");
-                } else {
-                    System.out.println("Operation failed");
-                }
+                StartUI.replace(input, tracker);
             } else if (select == 3) {
-                String id = input.askStr("Enter ID item which want to delete: ");
-                if (tracker.delete(id)) {
-                    System.out.println("Operation completed successfully");
-                } else {
-                    System.out.println("Operation failed");
-                }
+                StartUI.delete(input, tracker);
             } else if (select == 4) {
-                String id = input.askStr("Enter ID item: ");
-                System.out.println(tracker.findById(id).getName());
+                StartUI.findById(input, tracker);
             } else if (select == 5) {
-                System.out.print("Enter name item: ");
-                String name = input.askStr("Enter name item: ");
-                for (Item item : tracker.findByName(name)) {
-                    System.out.println(item.getName() + " " + item.getId());
-                }
+                StartUI.findByName(input, tracker);
             } else if (select == 6) {
                 run = false;
             }
+        }
+    }
+
+    /**
+     * Метод создает новый объект Item и добавляет его в tracker.
+     * @param input объект для чтения вводимых данных.
+     * @param tracker объект трекера.
+     */
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    /**
+     * Метод выводит все объекты Item в консоль.
+     * @param tracker объект трекера.
+     * @return
+     */
+    public static void showAllItem (Tracker tracker) {
+        System.out.println("=== All Item ====");
+        Item[] items = tracker.findAll();
+        for (Item item : items) {
+            System.out.println(item.getName() + " " + item.getId());
+        }
+    }
+
+    /**
+     * Метод редактирует Item.
+     * @param input объект для чтения вводимых данных.
+     * @param tracker объект трекера.
+     */
+    public static void replace(Input input, Tracker tracker) {
+        String id = input.askStr("Enter ID item which want to replace: ");
+        String name = input.askStr("Enter new name item: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Operation completed successfully");
+        } else {
+            System.out.println("Operation failed");
+        }
+    }
+
+    /**
+     * Метод удаляет заявку из трекера.
+     * @param input объект для чтения вводимых данных.
+     * @param tracker объект трекера.
+     */
+    public static void delete(Input input, Tracker tracker) {
+        String id = input.askStr("Enter ID item which want to delete: ");
+        if (tracker.delete(id)) {
+            System.out.println("Operation completed successfully");
+        } else {
+            System.out.println("Operation failed");
+        }
+    }
+
+    /**
+     * Метод ищет заявку по id.
+     * @param input объект для чтения вводимых данных.
+     * @param tracker объект трекера.
+     */
+    public static void findById(Input input, Tracker tracker) {
+        String id = input.askStr("Enter ID item: ");
+        System.out.println(tracker.findById(id).getName());
+    }
+
+    /**
+     * Метод ищет заявку по name.
+     * @param input объект для чтения вводимых данных.
+     * @param tracker объект трекера.
+     */
+    public static void findByName(Input input, Tracker tracker) {
+        System.out.print("Enter name item: ");
+        String name = input.askStr("Enter name item: ");
+        for (Item item : tracker.findByName(name)) {
+            System.out.println(item.getName() + " " + item.getId());
         }
     }
 
