b07e96db123ee2e4c072ad6ce4ff02d423228d4d
Slevkelebr <slevkelebr@yandex.ru>
2020-04-14 11:28:34 +0500

    Написать тесты на StartUI. [#785]

---
+++ src/main/java/ru/job4j/tracker/StubAction.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker;
+
+/**
+ * class StubAction - класс для тестирования StartUI.
+ *
+ * @author Sergey Frolov (slevkelebr@yandex.ru)
+ * @version 0.1
+ * @since 14.04.2020
+ */
+public class StubAction implements UserAction {
+    private boolean call = false;
+
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        call = true;
+        return false;
+    }
+
+    public boolean isCall() {
+        return call;
+    }
+}
